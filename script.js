f8ee66e56d814e4930f60445a49faa21671ae1e0
sharon <sharondoroth@live.com>
2023-04-30 22:30:46 +0300

    Pika Game

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
-const pika = document.querySelector('.pika');
-const poop = document.querySelector('.poop');
+const pika = document.querySelector(".pika");
+const poop = document.querySelector(".poop");
 
 const jump = () => {
-    document.getElementById("text-start").style.color = "white";
+  document.getElementById("text-start").style.color = "white";
 
-  pika.classList.add('jump');
+  pika.classList.add("jump");
 
   setTimeout(() => {
-    pika.classList.remove('jump');
+    pika.classList.remove("jump");
   }, 500);
 };
 
 const loop = setInterval(() => {
-
-    console.log('loop')
+  console.log("loop");
 
   const poopPosition = poop.offsetLeft;
-  const pikaPosition = +window.getComputedStyle(pika).bottom.replace(`px`, '');
+  const pikaPosition = +window.getComputedStyle(pika).bottom.replace(`px`, "");
 
   console.log(pikaPosition);
 
   if (poopPosition <= 120 && poopPosition > 0 && pikaPosition < 85) {
-
-    poop.style.animation = 'none';
+    poop.style.animation = "none";
     poop.style.left = `${poopPosition}px`;
 
     poop.src = "img/dance.gif";
     poop.style.width = "120px";
     poop.style.marginLeft = "30px";
-    
 
-    pika.style.animation = 'none';
+    pika.style.animation = "none";
     pika.style.bottom = `${pikaPosition}px`;
 
     pika.src = "img/pika2.gif";
-    pika.style.width = '120px'
-    pika.style.marginLeft = '30px'
+    pika.style.width = "120px";
+    pika.style.marginLeft = "30px";
 
     clearInterval(loop);
 
     document.getElementById("text-start").style.color = "black";
-    document.getElementById("text-start").innerHTML="<strong>GAME OVER</strong>";
-
+    document.getElementById("text-start").innerHTML =
+      "<strong>GAME OVER</strong>";
   }
 }, 10);
 
-document.addEventListener('keydown', jump);
+document.addEventListener("keydown", jump);
